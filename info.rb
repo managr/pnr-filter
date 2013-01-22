512acdc322ec915b207bb892688979b5493a4599
Marcin Naglik <marcin.naglik@gmail.com>
2013-01-22 21:06:34 +0100

    adding set size info

--- info.rb
+++ info.rb
@@ -4,6 +4,7 @@ r = Redis.new
 
 info = r.info
 
+puts "Pnr set contains #{r.scard('pnrs')} pnrs"
 puts "Used memory #{info['used_memory_human']}"
 puts "Peak used memory #{info['used_memory_peak_human']}"
 
